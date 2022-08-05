583af749c24bbe0fcb56703fb6a78ce4e4921cca
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2022-08-05 18:09:38 -0500

    login and register

---
+++ registro/registro.js
@@ -0,0 +1,11 @@
+let UserId = document.getElementById("contraseñaUnica");
+let numericoContraseña = document.querySelector(".cont-numerico-contraseña")
+UserId.addEventListener("click", function () {
+  numericoContraseña.classList.remove("no-display")
+})
+
+let repeatClave = document.getElementById("repeatClave");
+let numericoContraseñaRepeat = document.querySelector(".cont-numerico-repeat")
+repeatClave.addEventListener("click", function () {
+  numericoContraseñaRepeat.classList.remove("no-display")
+})
