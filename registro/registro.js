a3af08b48dffc76c20fb413b9e4f43655bff61ea
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2022-08-05 18:17:39 -0500

    modificación formulario registro

--- registro/registro.js
+++ registro/registro.js
@@ -2,10 +2,12 @@ let UserId = document.getElementById("contraseñaUnica");
 let numericoContraseña = document.querySelector(".cont-numerico-contraseña")
 UserId.addEventListener("click", function () {
   numericoContraseña.classList.remove("no-display")
+  numericoContraseñaRepeat.classList.add("no-display")
 })
 
 let repeatClave = document.getElementById("repeatClave");
 let numericoContraseñaRepeat = document.querySelector(".cont-numerico-repeat")
 repeatClave.addEventListener("click", function () {
   numericoContraseñaRepeat.classList.remove("no-display")
+  numericoContraseña.classList.add("no-display")
 })
