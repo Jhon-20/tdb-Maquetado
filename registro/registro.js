433a0e4b00493120b4dbeca0cc46124d2a1c12a5
Jhon <jasoplopuco@synopsis.onmicrosoft.com>
2022-08-05 20:32:14 -0500

    actualizacion formularios

--- registro/registro.js
+++ registro/registro.js
@@ -1,13 +1,18 @@
 let UserId = document.getElementById("contraseñaUnica");
-let numericoContraseña = document.querySelector(".cont-numerico-contraseña")
+let numericoContraseña = document.querySelector(".cont-numerico-contraseña");
+let repeatClave = document.getElementById("repeatClave");
+let numericoContraseñaRepeat = document.querySelector(".cont-numerico-repeat")
+let containerCard = document.querySelector(".container-card");
+
 UserId.addEventListener("click", function () {
   numericoContraseña.classList.remove("no-display")
   numericoContraseñaRepeat.classList.add("no-display")
+  containerCard.style.padding = "2em"
 })
 
-let repeatClave = document.getElementById("repeatClave");
-let numericoContraseñaRepeat = document.querySelector(".cont-numerico-repeat")
+
 repeatClave.addEventListener("click", function () {
   numericoContraseñaRepeat.classList.remove("no-display")
   numericoContraseña.classList.add("no-display")
+  containerCard.style.padding = "2em"
 })
